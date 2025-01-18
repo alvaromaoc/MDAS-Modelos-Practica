ef8c62c916e602afb1ef73f0a4b1cc580d12de86
Álvaro Mañoso Oca <alvaro.manoso@students.salle.url.edu>
2025-01-18 11:30:17 +0100

    Fix push pipeline

--- src/native-test/java/edu/url/salle/mdas/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package edu.url.salle.mdas;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
