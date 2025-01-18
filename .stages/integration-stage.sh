3f8331e4391a0ebf23a962bbd8f3c1cc0e52c012
Álvaro Mañoso Oca <alvaro.manoso@students.salle.url.edu>
2025-01-18 14:41:56 +0100

    Release behavior

--- .stages/integration-stage.sh
+++ .stages/integration-stage.sh
@@ -1,4 +1,6 @@
-./gradlew clean build \
+environment="$1"
+
+./gradlew clean build -Dquarkus.openshift.name="modelos-${environment}" \
   -Dquarkus.container-image.build=true \
   -Dquarkus.container-image.publish=true \
   -Dquarkus.openshift.deploy=true
