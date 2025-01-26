cd05db80764af088c2198875ccaadbc7b7fd8780
Álvaro Mañoso Oca <43934777+alvaromaoc@users.noreply.github.com>
2025-01-26 09:41:15 +0100

    Simplify pipelines (#43)
    Co-authored-by: Álvaro Mañoso Oca <alvaro.manoso@students.salle.url.edu>

--- cicd/application-deploy.sh
+++ cicd/application-deploy.sh
@@ -1,15 +1,4 @@
-environment="$1"
-version="$2"
-
-# Guard clause to ensure environment is one of the available
-case "$environment" in
-  develop|release|hotfix|production)
-    ;;
-  *)
-    echo "Error: Invalid environment '${environment}'"
-    exit 1
-    ;;
-esac
+version="$1"
 
 # Guard clause to ensure version is correctly formed with SemVer
 if ! echo "$version" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?$'; then
@@ -17,11 +6,12 @@ if ! echo "$version" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?$'; then
     exit 1
 fi
 
-echo "Deploying version '${version}' into '${environment}' environment"
+echo "Running DEPLOY pipeline"
+echo "Deploying version '${version}' into 'production' environment"
 
 ./gradlew clean build \
   -Pversion="${version}" \
   -Dquarkus.container-image.build=false \
   -Dquarkus.container-image.push=false \
   -Dquarkus.openshift.deploy=true \
-  -Dquarkus.openshift.name="modelos-${environment}"
+  -Dquarkus.openshift.name=modelos-production
